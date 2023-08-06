db952c36424c257849cf612f62a2c39d93d7888c
rolanos <ivankson@gmail.com>
2023-08-06 23:32:49 +0300

    Rework lifeindex

--- lib/pages/index_of_life.dart
+++ lib/pages/index_of_life.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:i_am_app/pages/bloc/bloc/user_bloc.dart';
 import 'package:i_am_app/classes/models/life_index.dart' as li;
@@ -49,204 +50,212 @@ class _LifeIndexState extends State<LifeIndex> {
     'Самоконтроль',
     'Уверенность в себе',
   ];
-  final _controller = TextEditingController();
+  final List<TextEditingController> controllers = [];
 
   @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
+  void initState() {
+    super.initState();
+    for (var i = 0; i < spheres.length; i++) {
+      controllers.add(TextEditingController());
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(top: 10.0),
-      child: BlocBuilder<UserBloc, UserState>(
-        builder: (context, state) {
-          if (state is UserInitial) {
-            List<double> buf = [];
-            for (var i = 0; i < 20; i++) {
-              buf.add(0);
-            }
-            li.LifeIndex data = li.LifeIndex(
-                date: DateTime(
-                  DateTime.now().year,
-                  indexMonth,
-                ),
-                indexes: buf);
-            if (state.user.index.isNotEmpty) {
-              for (var i in state.user.index) {
-                if (i.date.month == indexMonth) {
-                  data = i;
-                  break;
+    return BlocBuilder<UserBloc, UserState>(
+      builder: (context, state) {
+        if (state is UserInitial) {
+          List<double> buf = [];
+          for (var i = 0; i < 20; i++) {
+            buf.add(0);
+          }
+          li.LifeIndex data = li.LifeIndex(
+              date: DateTime(
+                DateTime.now().year,
+                indexMonth,
+              ),
+              indexes: buf);
+          for (var i = 0; i < controllers.length; i++) {
+            controllers[i].text = '0.0';
+          }
+          if (state.user.index.isNotEmpty) {
+            for (var i in state.user.index) {
+              if (i.date.month == indexMonth) {
+                data = i;
+                for (var j = 0; j < controllers.length; j++) {
+                  controllers[j].text = i.indexes?[j].toString() ?? '0.0';
                 }
+                break;
               }
             }
-            return Column(
-              children: [
-                Container(
-                    height: 110,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: months.length,
-                      itemBuilder: (context, index) {
-                        if (index == 0) {
-                          return SizedBox();
-                        }
-                        return Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 4.0),
-                          child: InkWell(
-                              child: Container(
-                                width: 110,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(16.0),
-                                    color: Colors.white),
-                                child: Center(
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Icon(Icons.add_a_photo_outlined),
-                                      Text(months[index - 1])
-                                    ],
-                                  ),
+          }
+          return Column(
+            children: [
+              Container(
+                  height: 110,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: months.length,
+                    itemBuilder: (context, index) {
+                      if (index == 0) {
+                        return SizedBox();
+                      }
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+                        child: InkWell(
+                            child: Container(
+                              width: 110,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(16.0),
+                                  color: Colors.white),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Builder(
+                                      builder: (context) {
+                                        double lifesum = 0.0;
+                                        final list = state.user.index;
+                                        for (var i = 0; i < list.length; i++) {
+                                          if (list[i].date.month == index) {
+                                            lifesum = list[i].lifeIndex;
+                                          }
+                                        }
+                                        return Text(
+                                          lifesum.toString(),
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium!
+                                              .copyWith(
+                                                  fontWeight: FontWeight.w700),
+                                        );
+                                      },
+                                    ),
+                                    Text(months[index - 1])
+                                  ],
                                 ),
                               ),
-                              onTap: () {
-                                for (var i in state.user.index) {
-                                  if (i.date.month == index) {
-                                    data = i;
-                                    break;
+                            ),
+                            onTap: () {
+                              for (var i in state.user.index) {
+                                if (i.date.month == index) {
+                                  data = i;
+                                  for (var j = 0; j < controllers.length; j++) {
+                                    controllers[j].text =
+                                        i.indexes?[j].toString() ?? '0.0';
                                   }
+                                  break;
                                 }
-                                setState(() {
-                                  indexMonth = index;
-                                });
-                              }),
-                        );
-                      },
-                    )),
-                Container(
-                  height: 60,
-                  child: Center(
-                      child: Text(months[indexMonth - 1],
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.w400))),
-                ),
-                Container(
-                  height: 100,
-                  child: Center(
-                      child: Text(
-                          (data != null)
-                              ? data!.lifeIndex.toStringAsFixed(1)
-                              : "-",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 72,
-                              fontWeight: FontWeight.w800))),
-                ),
-                SizedBox(
-                  height: 18.0,
-                ),
-                ElevatedButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll(Colors.white)),
-                    onPressed: () => context
-                        .read<UserBloc>()
-                        .add(UpdateIndex(phone: state.user.phone, index: data)),
+                              }
+                              setState(() {
+                                indexMonth = index;
+                              });
+                            }),
+                      );
+                    },
+                  )),
+              Container(
+                height: 60,
+                child: Center(
+                    child: Text(months[indexMonth - 1],
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400))),
+              ),
+              Container(
+                height: 100,
+                child: Center(
                     child: Text(
-                      'Сохранить',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    )),
-                Expanded(
-                    child: Padding(
-                  padding: EdgeInsets.all(20),
+                        (data != null)
+                            ? data!.lifeIndex.toStringAsFixed(1)
+                            : "-",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 72,
+                            fontWeight: FontWeight.w800))),
+              ),
+              Expanded(
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 24.0, vertical: 6.0),
                   child: Container(
                     child: ListView.builder(
-                        scrollDirection: Axis.vertical,
-                        itemCount: spheres.length,
-                        itemBuilder: (context, index) {
-                          return Card(
-                              child: ListTile(
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius:
-                                          BorderRadius.circular(15.0)),
-                                  title: Text(
-                                    spheres[index],
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 16.0),
-                                  ),
-                                  tileColor: Colors.white,
-                                  trailing: Container(
-                                    padding: EdgeInsets.symmetric(
-                                        horizontal: 8, vertical: 4),
-                                    decoration: BoxDecoration(
-                                      color: Color(0xFF252E29),
-                                      borderRadius: BorderRadius.circular(12),
-                                    ),
-                                    child: Text(
-                                      data.indexes![index].toString(),
-                                      style: TextStyle(
-                                          color: Colors.white, fontSize: 16.0),
-                                    ),
-                                  ),
-                                  onTap: () {
-                                    showDialog(
-                                        context: context,
-                                        builder: (context) {
-                                          return AlertDialog(
-                                            title: Text('Введите текст'),
-                                            content: TextFormField(
-                                              keyboardType:
-                                                  TextInputType.number,
-                                              // Вместо controller вы также можете использовать onChanged метод для получения значения введенного текста
-                                              controller: _controller,
-                                              style: Theme.of(context)
-                                                  .textTheme
-                                                  .bodyMedium,
-                                              decoration: InputDecoration(
-                                                  hintText: "Введите текст",
-                                                  hintStyle: TextStyle(
-                                                      color: Colors.grey)),
-                                            ),
-                                            actions: <Widget>[
-                                              ElevatedButton(
-                                                child: Text('СОХРАНИТЬ'),
-                                                onPressed: () {
-                                                  data.indexes![index] =
-                                                      double.tryParse(
-                                                              _controller
-                                                                  .text) ??
-                                                          data.indexes![index];
-                                                  context.read<UserBloc>().add(
-                                                      UpdateIndex(
-                                                          phone:
-                                                              state.user.phone,
-                                                          index: data));
-                                                  Navigator.of(context).pop();
-                                                },
-                                              )
-                                            ],
-                                          );
-                                        });
-                                    ;
-                                  }));
-                        }),
+                      scrollDirection: Axis.vertical,
+                      itemCount: spheres.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          child: ListTile(
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15.0)),
+                            title: Text(
+                              spheres[index],
+                              style: TextStyle(
+                                  color: Colors.black, fontSize: 16.0),
+                            ),
+                            tileColor: Colors.white,
+                            trailing: Container(
+                              width: 42,
+                              height: 30,
+                              decoration: BoxDecoration(
+                                color: Color(0xFF252E29),
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                              child: Center(
+                                child: TextFormField(
+                                  keyboardType: TextInputType.number,
+                                  controller: controllers[index],
+                                  textAlign: TextAlign.center,
+                                  inputFormatters: [
+                                    LengthLimitingTextInputFormatter(4),
+                                  ],
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none, hintText: '0'),
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 12.0),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ))
-              ],
-            );
-          } else {
-            return Center(
-              child: CircularProgressIndicator(
-                color: Colors.black,
+                ),
               ),
-            );
-          }
-        },
-      ),
+              SizedBox(
+                height: 4.0,
+              ),
+              ElevatedButton(
+                style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.white)),
+                onPressed: () {
+                  for (var i = 0; i < controllers.length; i++) {
+                    data.indexes?[i] =
+                        double.tryParse(controllers[i].text) ?? 0.0;
+                  }
+                  context
+                      .read<UserBloc>()
+                      .add(UpdateIndex(phone: state.user.phone, index: data));
+                },
+                child: Text(
+                  'Сохранить',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+              ),
+              SizedBox(
+                height: 4.0,
+              ),
+            ],
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.black,
+            ),
+          );
+        }
+      },
     );
   }
 }
